8f0f05bd1004bd34cd0d957c5e82ad9472bdca82
Di Majo Wilsfried <wilsfrieddm@gmail.com>
2024-01-25 15:02:42 +0100

    Login Page

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+import Nav from "../Components/Nav/Nav"
+import Footer from "../Components/Footer/Footer"
+import SignIn from "../Components/SignIn/SignIn"
+
+
 
 export default function Login() {
     return(
             <div>
-                
+                <Nav />
+                <main className="main bg-dark">
+                    <SignIn />
+                </main>
+                <Footer />
             </div>
     )
 }
